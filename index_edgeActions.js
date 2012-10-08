003b519873d99f1ed7ab66b22985acf1d7d02de0
Pierre Nerbring <pierre.nerbring@sigma.se>
2012-10-08 22:44:53 +0200

    Added project

---
+++ index_edgeActions.js
@@ -0,0 +1,54 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",4250,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Text_Login}","click",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2500,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Padlock2}","click",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Login-Button2}","click",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Alarm_button_hidden}","click",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",8250,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Remote_Control_Hidden}","click",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",10250,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Close_remote}","click",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",11000,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Back_hidden}","click",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",11500,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Back_hiddenCopy}","click",function(sym,e){sym.play(11501);});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",13500,function(sym,e){sym.stop(8250);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Remote_Control_HiddenCopy}","click",function(sym,e){sym.play(10251);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Turn_on_alarm_hidden}","click",function(sym,e){sym.play(13751);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Close_remoteCopy}","click",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",18250,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",18750,function(sym,e){sym.stop(11500);});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",6750,function(sym,e){sym.play(6750);});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",9000,function(sym,e){sym.stop(10250);});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",12250,function(sym,e){sym.stop(8250);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Hidden_Button_Center_Login}","click",function(sym,e){sym.play();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-185781534");
